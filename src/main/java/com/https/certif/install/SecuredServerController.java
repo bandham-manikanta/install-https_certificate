87c0bab489040a1f1271df1fcd0ea2c945266d59
Bandham Manikanta <bandham.manikanta@centurylink.com>
2020-04-26 14:33:26 +0530

    added run_application.bat & api method to POST

--- src/main/java/com/https/certif/install/SecuredServerController.java
+++ src/main/java/com/https/certif/install/SecuredServerController.java
@@ -1,7 +1,5 @@
 package com.https.certif.install;
 
-import java.util.Date;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecuredServerController {
 
     @RequestMapping(value = "/https/secured", method = {
-            RequestMethod.GET }, produces = MediaType.APPLICATION_JSON_VALUE)
+            RequestMethod.POST }, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> sendStatusCode400() {
         String message = "{\"message\":\"It is secured HTTPS protocal API\"}";
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
